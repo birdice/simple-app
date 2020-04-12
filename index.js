3830174ed94b81a8da4666848305dacb1ab21799
birdice <63162105+birdice@users.noreply.github.com>
2020-04-12 15:43:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express');
-const app = express ();
+var express = require("express");
+var app = express();
+var MongoClient = require("mongodb").MongoClient;
 
-app.get('/', (req, res) =>res.send('Hello Jairo Solorzano - UNIR Practica Packer'));
+app.get("/", function(req, res) {
+  res.send("Hello Jairo Solorzano - UNIR Practica Packer");
+});
 
-app.listen(3000);
-console.log('Server on port', 3000);
+app.get("/users", function() {  
+  MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+    if (err) next   
+    db
+      .collection("users")  
+      .find()   
+      .toArray(function(err, result) {
+        if (err) throw err;  
+        console.log('Mongo connected')
+        res.json(result)
+      });   
+  });  
+}); 
+
+app.listen(3000,function(){  
+    console.log('Express app start on port 3000') 
+}); 
